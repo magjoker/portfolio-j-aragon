2a0e3e96bd1f3d4dedecd631719b172c88e52815
Jared Aragon <jaredaragon7@gmail.com>
2024-01-03 22:43:19 -0700

    Simplified the landing page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return(
         <div className="bigfeet">
             {/* all I have to show right now is my github, more will come soon */}
-            <a href="https://github.com/magjoker" className="git-link">GitHub Account</a>
+            <a href="https://gist.github.com/magjoker/5ff36b265952b2026352f174c6d5c107" className="git-link">GitHub Gist written about Regular Expressions</a>
         </div>
     )
 }
